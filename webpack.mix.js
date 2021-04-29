02335a75a88630eb1e9411228ae5bfc13282aa40
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2021-04-29 21:38:49 +0300

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,35 +1,60 @@
 let mix = require('laravel-mix');
-mix.pug = require('laravel-mix-pug');
+mix.pug = require('laravel-mix-pug-recursive');
+require('laravel-mix-imagemin');
 
 // todo pug update
 
 mix
     .setPublicPath('docs')
     .setResourceRoot('../')
-    .copyDirectory('resources/images/*.*', 'docs/images')
-    // .copyDirectory('resources/favicons/*.*', 'docs/favicons')
     .js('resources/scripts/app.js', 'js/scripts.js')
     .sass('resources/styles/app.scss', 'css/styles.css')
     .sass('resources/styles/customer.scss', 'template.css')
     // .options({
     //     postCss: [
     //         require('tailwindcss')('tailwind.config.js'),
-    //     ]
+    //     ],
     // })
-    .pug('resources/views/pages/*.pug', '../../../docs', {
+    .pug('resources/views/pages/*.pug', 'docs', {
+        excludePath: 'resources/views/pages',
         pug: {
-            pretty: true
-        }
+            pretty: true,
+        },
     })
 ;
 
 if (mix.inProduction()) {
-    //
-} else {
-    mix.browserSync({
-        server: "./docs",
-        files: [
-            'resources/**/*.*',
+    mix.imagemin(
+        [
+            'images/**.*',
+            'favicons/**.*',
         ],
-    });
+        {
+            context: 'resources',
+        },
+        {
+            optipng: {
+                optimizationLevel: 5,
+            },
+            jpegtran: null,
+            plugins: [
+                require('imagemin-mozjpeg')({
+                    quality: 100,
+                    progressive: true,
+                }),
+            ],
+        },
+    );
+}
+else {
+    mix
+        .copyDirectory('resources/images/*.*', 'docs/images')
+        .copyDirectory('resources/favicons/*.*', 'docs/favicons')
+        .browserSync({
+            server: './docs',
+            files: [
+                'resources/**/*.*',
+            ],
+        })
+    ;
 }
